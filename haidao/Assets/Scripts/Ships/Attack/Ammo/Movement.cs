451a42fe934f861b7fbaa338392ac5a2ca340771
Arturet <artjimlop@gmail.com>
2017-04-05 12:46:15 +0200

    Corrected direction spawning

--- haidao/Assets/Scripts/Ships/Attack/Ammo/Movement.cs
+++ haidao/Assets/Scripts/Ships/Attack/Ammo/Movement.cs
@@ -18,6 +18,6 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		theTransform.position += theTransform.forward * maxSpeed * Time.deltaTime;
+		theTransform.position += theTransform.right * maxSpeed * Time.deltaTime;
 	}
 }
